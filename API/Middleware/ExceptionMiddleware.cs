1b397ed899f4a5d9256b7485c0b0106dd00f6d85
Alexey Barnyshev <alexeibarnyshev@yandex.ru>
2026-04-20 18:18:26 +0500

    feat new exceptions in bagcontroller

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -24,8 +24,14 @@ public class ExceptionMiddleware(IHostEnvironment env, RequestDelegate next)
         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
         var response = env.IsDevelopment()
-        ? new ApiErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace)
-        : new ApiErrorResponse(context.Response.StatusCode, ex.Message, "Internal Server Error");
+       ? new ApiErrorResponse(
+           context.Response.StatusCode,
+           ex.InnerException?.Message ?? ex.Message,
+           ex.StackTrace)
+       : new ApiErrorResponse(
+           context.Response.StatusCode,
+           "Internal Server Error",
+           null);
 
         var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
         var json = JsonSerializer.Serialize(response, options);
